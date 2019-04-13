fccd0af3ca7caedd57dcdbe7827a0831610be77f
Samuel Sirois <samuel@sirois.info>
2019-04-12 23:42:59 -0400

    Add electron

---
+++ main.ts
@@ -0,0 +1,10 @@
+import { app, BrowserWindow } from 'electron';
+
+function createWindow() {
+  const applicationMainWindow = new BrowserWindow({width: 800; height: 600});
+
+  applicationMainWindow.loadFile('./dist/index.html');
+}
+
+app.on('ready', createWindow);
+
