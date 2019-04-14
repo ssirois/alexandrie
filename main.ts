0d4ffe4f1b3194d531b7474cee8e174c90001619
Samuel Sirois <samuel@sirois.info>
2019-04-14 16:57:57 -0400

    Fix electron app
    A commit was missing from the previous merge.

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { app, BrowserWindow } from 'electron';
 
 function createWindow() {
-  const applicationMainWindow = new BrowserWindow({width: 800; height: 600});
+  const applicationMainWindow = new BrowserWindow({width: 800, height: 600});
 
   applicationMainWindow.loadFile('./dist/index.html');
 }
